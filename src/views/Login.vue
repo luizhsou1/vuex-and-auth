e785e1d73d2eca58895c1cfb5404bde36b3acb3e
Luiz Henrique <luihsou1@gmail.com>
2020-08-06 15:33:14 -0300

    implementado login e logout atraves do localstorage

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <h1>Login</h1>
+        <form @submit.prevent="efetuarLogin">
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" v-model="usuario.email">
+            </div>
+            <div class="form-group">
+                <label for="senha">E-mail</label>
+                <input type="password" class="form-control" v-model="usuario.senha">
+            </div>
+            <button type="submit" class="btn btn-primary brn-block">Logar</button>
+            <router-link :to="{ name: 'novo.usuario' }">
+                cadastre-se aqui!
+            </router-link>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            usuario: {
+                email: '',
+                senha: '',
+            }
+        }
+    },
+    methods: {
+        efetuarLogin() {
+            axios.post('http://localhost:8000/auth/login', this.usuario)
+                .then(response => { 
+                    console.log(response)
+                    localStorage.setItem('token', response.data.access_token)
+                    this.$router.push({name: 'gerente'})
+                })
+                .catch(error => console.log(error))
+        }
+    }
+}
+</script>
